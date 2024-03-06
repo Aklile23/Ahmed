1ea2e1345a03aceb7189cdbc388672ba9890d4e4
Aklile <akliletilahun@gmail.com>
2024-03-06 16:21:27 +0300

    first commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect } from 'react';
+import Section from './Section';
+import Heading from './Heading';
+import Ahmed from '../assets/Ahmed.mp4'
+
+const Demo = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const entry = entries[0];
+                if (entry.isIntersecting) {
+                    videoRef.current.play();
+                } else {
+                    videoRef.current.pause();
+                }
+            },
+            { threshold: 0.5 } // Adjust this value based on how much of the video should be in view to autoplay
+        );
+
+        if (videoRef.current) {
+            observer.observe(videoRef.current);
+        }
+
+        return () => {
+            if (videoRef.current) {
+                observer.unobserve(videoRef.current);
+            }
+        };
+    }, []);
+    return (
+        <Section id="demo">
+            <div className='container mt-6'>
+                <Heading
+                    tag="Intro"
+                    title="Who is Ahmed?"
+                />
+                <video className="h-full w-full rounded-lg" controls ref={videoRef}>
+                    <source src={Ahmed} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+
+            </div>
+        </Section>
+      );
+}
+
+export default Demo;
---
+++ src/components/Services.jsx
@@ -0,0 +1,130 @@
+//Features
+import Section from "./Section";
+import Heading from "./Heading";
+import { service1, service2, service3, check,one, AhmedLogo, AhmedBackground, AhmedBackground2, two } from "../assets";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
+
+import Generating from "./Generating";
+
+const Services = () => {
+  return (
+    <Section id="how-to-use">
+      <div className="mt-10 container">
+        <Heading
+          tag="FEATURES"
+          title="Ahmed in Focus: Zooming in on Key Features"
+        />
+
+        <div className="relative">
+          <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+              <img
+                className="w-full h-full object-cover md:object-right"
+                width={800}
+                alt="Smartest AI"
+                height={730}
+                src={one}
+              />
+            </div>
+
+            <div className="relative z-1 max-w-[23rem] ml-auto">
+              <h4 className="h4 mb-4">Multilingual Conversations</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Ahmed: Mastering Multiple Languages for Seamless Global Communication.
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img width={24} height={24} src={check} />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg-right-auto lg:bottom-8 lg:-translate-x-1/2" /> */}
+          </div>
+
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+              <div className="absolute inset-0">
+                <img
+                  src={AhmedBackground2}
+                  className="h-full w-full object-cover"
+                  width={630}
+                  height={750}
+                  alt="robot"
+                />
+              </div>
+
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Natural Language Understanding</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Ahmed comprehends with precision, using advanced natural language processing to 
+                  deliver accurate and context-aware responses, just like a human.
+                </p>
+              </div>
+
+              <PhotoChatMessage />
+            </div>
+
+            <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+              <div className="py-12 px-4 xl:px-8">
+                <h4 className="h4 mb-4">Flexible Customization</h4>
+                <p className="body-2 mb-[2rem] text-n-3">
+                  Ahmed: A Fully Customizable Experience, from Knowledge Base to Avatar, Tailored to Your Preferences.
+                </p>
+
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`rounded-2xl flex items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                      <div
+                        className={
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }
+                      >
+                        <img src={item} width={24} height={24} alt={item} />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                <img
+                  src={two}
+                  className="w-full h-full object-cover"
+                  width={520}
+                  height={400}
+                  alt="Scary robot"
+                />
+              </div>
+            </div>
+          </div>
+
+          <Gradient />
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Services;
